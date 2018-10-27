3b33e3b22a4c9b38189061efb2c08b41a20712ca
kiratae <kirataetwo@gmail.com>
2018-10-27 22:21:38 +0700

    update index, nearest_hospital, specific hospital, some picture, js, css and add new file review cure, review health, review nutrition

--- js/chatbot.js
+++ js/chatbot.js
@@ -8,14 +8,16 @@ $().ready(function(){
             $('.chat-box').addClass('chat-box-active')
             $(this).addClass('chat-btn-active')
             sleep(700).then(() => {
-                $('.arrow').text('expand_more')
+                $('.arrow').removeClass('fa-angle-up')
+                $('.arrow').addClass('fa-angle-down')
             })
         } else {
             $('.chat-box').removeClass('chat-box-active')
             $(this).removeClass('chat-btn-active')
             sleep(700).then(() => {
                 $('.chat-footer').hide()
-                $('.arrow').text('expand_less')
+                $('.arrow').addClass('fa-angle-up')
+                $('.arrow').removeClass('fa-angle-down')
             })
         }
     })
